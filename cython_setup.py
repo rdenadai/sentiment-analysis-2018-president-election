b6b5bceafe51abfd8d76536b32945c63200ff727
Rodolfo De Nadai <rdenadai@gmail.com>
2018-11-24 18:48:41 -0200

    Limpeza

--- cython_setup.py
+++
@@ -1,8 +0,0 @@
-from distutils.core import setup, Extension
-from Cython.Build import cythonize
-import numpy
-
-setup(
-    ext_modules=cythonize("src/ai/unsupervised/emotional_lsa_utils.pyx"),
-    include_dirs=[numpy.get_include()]
-)
