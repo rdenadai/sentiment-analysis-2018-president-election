bdafab70361accb88606cd71cc2f733931913bb2
Rodolfo De Nadai <rdenadai@gmail.com>
2018-11-27 23:28:00 -0200

    - Nova execução dos testes, apresentando mais dados para possíveis relatórios;  - Ajustes no site de demonstração.

--- src/site/config.py
+++ src/site/config.py
@@ -1,5 +1,7 @@
 from starlette.applications import Starlette
+from starlette.middleware.gzip import GZipMiddleware
 from starlette.staticfiles import StaticFiles
 
 app = Starlette(debug=False, template_directory='src/site/templates')
+app.add_middleware(GZipMiddleware, minimum_size=500)
 app.mount('/static', StaticFiles(directory='src/site/media'), name='static')
